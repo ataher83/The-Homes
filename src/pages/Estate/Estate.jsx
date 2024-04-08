2308cb05df369b5280b5654e26f694b2785dca57
ataher83 <taher83@gmail.com>
2024-04-08 21:26:28 +0600

    react icons used & Estate Section updated

--- src/pages/Estate/Estate.jsx
+++ src/pages/Estate/Estate.jsx
@@ -1,4 +1,6 @@
-
+import { ImLocation2 } from "react-icons/im";
+import { TfiRulerAlt2 } from "react-icons/tfi";
+import { IoLogoUsd } from "react-icons/io";
 
 const Estate = ({estate}) => {
     const { id, estate_title, segment_name, description, price, status, area, location, facilities, image, } = estate;
@@ -6,43 +8,42 @@ const Estate = ({estate}) => {
     return (
             
         <div className="card bg-base-100 shadow-xl mt-4">
+
                 <figure><img className="w-full h-72" src={image} alt="Shoes" />
                 </figure>
 
-                <div className="card-actions justify-between px-2 ">
-
-                    <p>Location: {location}</p>
-                    <p>Area: {area}</p>
+                <div className="flex justify-between px-2 pt-1 font-semibold">
+                    <p className="flex items-center"><TfiRulerAlt2 className="text-blue-400" /> {area}</p>
+                    <p className="flex items-center"><IoLogoUsd className="text-blue-400" /> {price}</p>
+                    <p className="bg-orange-400 rounded  capitalize px-1">For <span >{status}</span></p>
                 </div>
 
                 <div className="card-body px-1">
-
-                    <div className="flex gap-2 items-center">
+                    <div className="flex gap-2 items-center justify-center">
                         <div className="avatar pt-2">
                             <div className="w-4 rounded-full ring ring-info ring-offset-base-100 ring-offset-2">
                                 <p className="text-center font-semibold -mt-1 ">{id}</p>
                             </div>
                         </div>
-
                         <h2 className="card-title">
-                    {estate_title}
-                    <div className="badge badge-secondary">{segment_name}</div>
-                    </h2>
+                            {estate_title}
+                            <div className="badge badge-secondary">{segment_name}</div>
+                        </h2>
                     </div>
-
-                    
+                        
+                    <p className="flex items-center font-normal text-blue-500 pb-2"><ImLocation2 className="text-blue-400" />{location}</p>
+                        
                     <p>{description}</p>
 
-                    <div className="card-actions justify-end items-center">
-                        <p>Facilities:</p>
-                        <div className="badge badge-outline">{facilities[0]}</div> 
-                        <div className="badge badge-outline">{facilities[1]}</div> 
-                        <div className="badge badge-outline">{facilities[2]}</div> 
+                    <p className="text-center font-semibold ">Facilities:</p>
+                    <div className="card-actions justify-center items-center">
+                        <div className="badge badge-outline bg-blue-400 font-semibold text-white border-blue-500">{facilities[0]}</div> 
+                        <div className="badge badge-outline  bg-blue-400 font-semibold text-white border-blue-500">{facilities[1]}</div> 
+                        <div className="badge badge-outline  bg-blue-400 font-semibold text-white border-blue-500">{facilities[2]}</div> 
                     </div>
 
                     <div className="card-actions justify-between ">
-                        <p>Price: {price}</p>
-                        <p>Status: {status}</p>
+                        
                     </div>
                 </div>
         </div>
