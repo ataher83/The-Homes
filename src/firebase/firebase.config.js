70f443d705010b0e3fe420aa556c41fbfc27e180
ataher83 <taher83@gmail.com>
2024-04-09 18:48:04 +0600

    Env Variables applied to secure firebase config, connfig will not go to github

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,25 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBaXNWzQhnDyJJtHE2cRZEcMkokFyXV7aU",
-  authDomain: "the-homes-7063c.firebaseapp.com",
-  projectId: "the-homes-7063c",
-  storageBucket: "the-homes-7063c.appspot.com",
-  messagingSenderId: "410547926647",
-  appId: "1:410547926647:web:dc64aa35544b1fc0a4b872"
+
+
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+
+
+
+  // apiKey: "AIzaSyBaXNWzQhnDyJJtHE2cRZEcMkokFyXV7aU",
+  // authDomain: "the-homes-7063c.firebaseapp.com",
+  // projectId: "the-homes-7063c",
+  // storageBucket: "the-homes-7063c.appspot.com",
+  // messagingSenderId: "410547926647",
+  // appId: "1:410547926647:web:dc64aa35544b1fc0a4b872"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app; 
+export default app;
