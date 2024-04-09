c0c91a45d4482f80454f57fe151b3f6b5a67a3e6
ataher83 <taher83@gmail.com>
2024-04-09 17:36:59 +0600

    useLocation() applied so that user will go automatically to clicked destination private-routed page after signin.

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ({children}) => {
     const { user, loading } = useContext(AuthContext); 
+    const location = useLocation();
 
     if (loading) {
         return <div className="text-info flex items-center">
@@ -22,7 +23,7 @@ const PrivateRoute = ({children}) => {
         return children;
     }
 
-    return <Navigate to='/login'></Navigate>
+    return <Navigate state={location.pathname} to='/login'></Navigate>
     
 };
 
